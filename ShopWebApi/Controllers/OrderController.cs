a287fc2ec79e304a45a0d55e73e8d6459acc8682
XiangWeiqi <xiangweiqi666@gamil.com>
2023-06-11 00:28:00 +0800

    add order controller

---
+++ ShopWebApi/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopWebApi.Controllers
+{
+    public class OrderController_ : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
